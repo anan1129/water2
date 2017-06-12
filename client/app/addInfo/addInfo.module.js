eb436e2eb08782f530df7fbed16591f344f64d75
zhengan <anan1129@126.com>
2017-06-12 14:41:11 +0800

    'fix:信息插件新加两功能导致注入报错问题'

--- client/app/addInfo/addInfo.module.js
+++ client/app/addInfo/addInfo.module.js
@@ -5,149 +5,151 @@
     'use strict';
 
     angular.module('app.addInfo', ['app.addInfo.controller', 'app.addInfo.state', 'angularFileUpload', 'textAngular','app.addInfo.directive'])
-        // .config(['$provide',function ($provide) {
-        //     $provide.decorator('taOptions', ['taRegisterTool', 'taOptions','taSelection', function (taRegisterTool,taOptions,taSelection) {
-        //         // $delegate is the taOptions we are decorating
-        //         // register the tool with textAngular
-        //
-        //         taRegisterTool('backgroundColor', {
-        //             display: "<div spectrum-colorpicker ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></div>",
-        //             action: function (color) {
-        //                 var me = this;
-        //                 if (!this.$editor().wrapSelection) {
-        //                     setTimeout(function () {
-        //                         me.action(color);
-        //                     }, 100)
-        //                 } else {
-        //                     return this.$editor().wrapSelection('backColor', color);
-        //                 }
-        //             },
-        //             options: {
-        //                 replacerClassName: 'fa fa-paint-brush', showButtons: false
-        //             },
-        //             color: "#fff"
-        //         });
-        //         taRegisterTool('fontColor', {
-        //             display: "<spectrum-colorpicker trigger-id='{{trigger}}' ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></spectrum-colorpicker>",
-        //             action: function (color) {
-        //                 var me = this;
-        //                 if (!this.$editor().wrapSelection) {
-        //                     setTimeout(function () {
-        //                         me.action(color);
-        //                     }, 100)
-        //                 } else {
-        //                     return this.$editor().wrapSelection('foreColor', color);
-        //                 }
-        //             },
-        //             options: {
-        //                 replacerClassName: 'fa fa-font', showButtons: false
-        //             },
-        //             color: "#000"
-        //         });
-        //
-        //
-        //         taRegisterTool('fontName', {
-        //             display: "<span class='bar-btn-dropdown dropdown'>" +
-        //             "<button class='btn btn-blue dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-font'></i><i class='fa fa-caret-down'></i></button>" +
-        //             "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-family: {{o.css}}; width: 100%' type='button' ng-click='action($event, o.css)'><i ng-if='o.active' class='fa fa-check'></i>{{o.name}}</button></li></ul></span>",
-        //             action: function (event, font) {
-        //                 //Ask if event is really an event.
-        //                 if (!!event.stopPropagation) {
-        //                     //With this, you stop the event of textAngular.
-        //                     event.stopPropagation();
-        //                     //Then click in the body to close the dropdown.
-        //                     $("body").trigger("click");
-        //                 }
-        //                 return this.$editor().wrapSelection('fontName', font);
-        //             },
-        //             options: [
-        //                 {name: 'Sans-Serif', css: 'Arial, Helvetica, sans-serif'},
-        //                 {name: 'Serif', css: "'times new roman', serif"},
-        //                 {name: 'Wide', css: "'arial black', sans-serif"},
-        //                 {name: 'Narrow', css: "'arial narrow', sans-serif"},
-        //                 {name: 'Comic Sans MS', css: "'comic sans ms', sans-serif"},
-        //                 {name: 'Courier New', css: "'courier new', monospace"},
-        //                 {name: 'Garamond', css: 'garamond, serif'},
-        //                 {name: 'Georgia', css: 'georgia, serif'},
-        //                 {name: 'Tahoma', css: 'tahoma, sans-serif'},
-        //                 {name: 'Trebuchet MS', css: "'trebuchet ms', sans-serif"},
-        //                 {name: "Helvetica", css: "'Helvetica Neue', Helvetica, Arial, sans-serif"},
-        //                 {name: 'Verdana', css: 'verdana, sans-serif'},
-        //                 {name: 'Proxima Nova', css: 'proxima_nova_rgregular'}
-        //             ]
-        //         });
-        //
-        //
-        //         taRegisterTool('fontSize', {
-        //             display: "<span class='bar-btn-dropdown dropdown'>" +
-        //             "<button class='btn btn-default dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-text-width'></i><i class='fa fa-caret-down'></i></button>" +
-        //             "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-size: {{o.css}}; width: 100%' type='button' ng-click='action($event, o.value)'><i ng-if='o.active' class='fa fa-check'></i> {{o.name}}</button></li></ul>" +
-        //             "</span>",
-        //             action: function (event, size) {
-        //                 //Ask if event is really an event.
-        //                 if (!!event.stopPropagation) {
-        //                     //With this, you stop the event of textAngular.
-        //                     event.stopPropagation();
-        //                     //Then click in the body to close the dropdown.
-        //                     $("body").trigger("click");
-        //                 }
-        //                 // console.log(this.$editor().wrapSelection('fontSize',size));
-        //
-        //
-        //                 return this.$editor().wrapSelection('fontSize',size);
-        //             },
-        //             options: [
-        //                 {name: '12', css: 'xx-small', value: '14px'},
-        //                 {name: '13', css: 'x-small', value: 2},
-        //                 {name: '16', css: 'small', value: 3},
-        //                 {name: '18', css: 'medium', value: 4},
-        //                 {name: '25', css: 'large', value: 5},
-        //                 {name: '32', css: 'x-large', value: 6},
-        //                 {name: '48', css: 'xx-large', value: 7}
-        //             ]
-        //         });
-        //
-        //         taRegisterTool('lineHeight', {
-        //             display: "<span class='bar-btn-dropdown dropdown'>" +
-        //             "<button class='btn btn-default dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-text-height'></i><i class='fa fa-caret-down'></i></button>" +
-        //             "<ul class='dropdown-menu' style='width:180px;height:200px;overflow:auto;'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' type='button' ng-click='action($event, o.css)'><i ng-if='o.active' class='fa fa-check'></i> {{o.name}}</button></li></ul>" +
-        //             "</span>",
-        //             action: function (event, size) {
-        //                 //Ask if event is really an event.
-        //                 if (!!event.stopPropagation) {
-        //                     //With this, you stop the event of textAngular.
-        //                     event.stopPropagation();
-        //                     //Then click in the body to close the dropdown.
-        //                     $("body").trigger("click");
-        //                 }
-        //                 // console.log(this.$editor().wrapSelection('fontSize',size));
-        //                 var ele=taSelection.getSelection().container;
-        //                 $(ele).children().css('line-height','auto');
-        //                 $(ele).children().css('line-height',size)
-        //                 $(ele).css('line-height',size)
-        //
-        //
-        //             },
-        //             options: setOptions(1,50)
-        //         });
-        //
-        //         function setOptions(start,end){
-        //             var arr=[];
-        //             for(var i=start;i<=end;i++){
-        //                 var obj={name:i,css:i+'px'};
-        //                 arr.push(obj);
-        //             }
-        //             return arr;
-        //         }
-        //
-        //
-        //
-        //
-        //         // add the button to the default toolbar definition
-        //         // taOptions.toolbar[1].push('backgroundColor', 'fontColor', 'fontName', 'fontSize');
-        //         taOptions.toolbar[3].push('fontSize','lineHeight');
-        //         return taOptions;
-        //     }]);
-        // }])
+        .config(['$provide',function ($provide) {
+            $provide.decorator('taOptions', ['taRegisterTool', '$delegate','taSelection', 'taBrowserTag', 'taTranslations',
+                'taToolFunctions', function (taRegisterTool, taOptions,taSelection,taBrowserTag,taTranslations,taToolFunctions) {
+                    // $delegate is the taOptions we are decorating
+                    // register the tool with textAngular
+
+                    taRegisterTool('backgroundColor', {
+                        display: "<div spectrum-colorpicker ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></div>",
+                        action: function (color) {
+                            var me = this;
+                            if (!this.$editor().wrapSelection) {
+                                setTimeout(function () {
+                                    me.action(color);
+                                }, 100)
+                            } else {
+                                return this.$editor().wrapSelection('backColor', color);
+                            }
+                        },
+                        options: {
+                            replacerClassName: 'fa fa-paint-brush', showButtons: false
+                        },
+                        color: "#fff"
+                    });
+                    taRegisterTool('fontColor', {
+                        display: "<spectrum-colorpicker trigger-id='{{trigger}}' ng-model='color' on-change='!!color && action(color)' format='\"hex\"' options='options'></spectrum-colorpicker>",
+                        action: function (color) {
+                            var me = this;
+                            if (!this.$editor().wrapSelection) {
+                                setTimeout(function () {
+                                    me.action(color);
+                                }, 100)
+                            } else {
+                                return this.$editor().wrapSelection('foreColor', color);
+                            }
+                        },
+                        options: {
+                            replacerClassName: 'fa fa-font', showButtons: false
+                        },
+                        color: "#000"
+                    });
+
+
+                    taRegisterTool('fontName', {
+                        display: "<span class='bar-btn-dropdown dropdown'>" +
+                        "<button class='btn btn-blue dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-font'></i><i class='fa fa-caret-down'></i></button>" +
+                        "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-family: {{o.css}}; width: 100%' type='button' ng-click='action($event, o.css)'><i ng-if='o.active' class='fa fa-check'></i>{{o.name}}</button></li></ul></span>",
+                        action: function (event, font) {
+                            //Ask if event is really an event.
+                            if (!!event.stopPropagation) {
+                                //With this, you stop the event of textAngular.
+                                event.stopPropagation();
+                                //Then click in the body to close the dropdown.
+                                $("body").trigger("click");
+                            }
+                            return this.$editor().wrapSelection('fontName', font);
+                        },
+                        options: [
+                            {name: 'Sans-Serif', css: 'Arial, Helvetica, sans-serif'},
+                            {name: 'Serif', css: "'times new roman', serif"},
+                            {name: 'Wide', css: "'arial black', sans-serif"},
+                            {name: 'Narrow', css: "'arial narrow', sans-serif"},
+                            {name: 'Comic Sans MS', css: "'comic sans ms', sans-serif"},
+                            {name: 'Courier New', css: "'courier new', monospace"},
+                            {name: 'Garamond', css: 'garamond, serif'},
+                            {name: 'Georgia', css: 'georgia, serif'},
+                            {name: 'Tahoma', css: 'tahoma, sans-serif'},
+                            {name: 'Trebuchet MS', css: "'trebuchet ms', sans-serif"},
+                            {name: "Helvetica", css: "'Helvetica Neue', Helvetica, Arial, sans-serif"},
+                            {name: 'Verdana', css: 'verdana, sans-serif'},
+                            {name: 'Proxima Nova', css: 'proxima_nova_rgregular'}
+                        ]
+                    });
+
+
+                    taRegisterTool('fontSize', {
+                        display: "<span class='bar-btn-dropdown dropdown'>" +
+                        "<button class='btn btn-default dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-text-width'></i><i class='fa fa-caret-down'></i></button>" +
+                        "<ul class='dropdown-menu'><li ng-repeat='o in options'><button class='btn btn-blue checked-dropdown' style='font-size: {{o.css}}; width: 100%' type='button' ng-click='action($event, o.value)'><i ng-if='o.active' class='fa fa-check'></i> {{o.name}}</button></li></ul>" +
+                        "</span>",
+                        action: function (event, size) {
+                            //Ask if event is really an event.
+                            if (!!event.stopPropagation) {
+                                //With this, you stop the event of textAngular.
+                                event.stopPropagation();
+                                //Then click in the body to close the dropdown.
+                                $("body").trigger("click");
+                            }
+                            // console.log(this.$editor().wrapSelection('fontSize',size));
+
+
+                            return this.$editor().wrapSelection('fontSize',size);
+                        },
+                        options: [
+                            {name: '12', css: 'xx-small', value: '14px'},
+                            {name: '13', css: 'x-small', value: 2},
+                            {name: '16', css: 'small', value: 3},
+                            {name: '18', css: 'medium', value: 4},
+                            {name: '25', css: 'large', value: 5},
+                            {name: '32', css: 'x-large', value: 6},
+                            {name: '48', css: 'xx-large', value: 7}
+                        ]
+                    });
+
+                    taRegisterTool('lineHeight', {
+                        display: "<span class='bar-btn-dropdown dropdown'>" +
+                        "<button class='btn btn-default dropdown-toggle' type='button' ng-disabled='showHtml()' style='padding-top: 4px'><i class='fa fa-text-height'></i><i class='fa fa-caret-down'></i></button>" +
+                        "<ul class='dropdown-menu' style='min-width:145px;height:200px;overflow:auto;'><li ng-repeat='o in options' ><button class='btn btn-blue checked-dropdown' style='width:41px;' type='button' ng-click='action($event, o.css)'><i ng-if='o.active' class='fa fa-check'></i> {{o.name}}</button></li></ul>" +
+                        "</span>",
+                        action: function (event, size) {
+                            //Ask if event is really an event.
+                            if (!!event.stopPropagation) {
+                                //With this, you stop the event of textAngular.
+                                event.stopPropagation();
+                                //Then click in the body to close the dropdown.
+                                $("body").trigger("click");
+                            }
+                            // console.log(this.$editor().wrapSelection('fontSize',size));
+                            var ele=taSelection.getSelection().container||'';
+
+                            $(ele).children().css('line-height','auto');
+                            $(ele).children().css('line-height',size)
+                            $(ele).css('line-height',size)
+
+
+                        },
+                        options: setOptions(1,50)
+                    });
+
+                    function setOptions(start,end){
+                        var arr=[];
+                        for(var i=start;i<=end;i++){
+                            var obj={name:i,css:i+'px'};
+                            arr.push(obj);
+                        }
+                        return arr;
+                    }
+
+
+
+
+                    // add the button to the default toolbar definition
+                    // taOptions.toolbar[1].push('backgroundColor', 'fontColor', 'fontName', 'fontSize');
+                    taOptions.toolbar[3].push('fontSize','lineHeight');
+                    return taOptions;
+                }]);
+        }])
     ;
 })();
